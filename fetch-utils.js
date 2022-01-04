50075b067c25e88e79e2a75695ffe479ded3c29a
Benatovich <sambenatovich@gmail.com>
2022-01-04 18:53:40 -0500

    all update functions look good

--- fetch-utils.js
+++ fetch-utils.js
@@ -3,10 +3,98 @@ const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsI
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+export async function fetchCity() {
+    const response = await client
+        .from('cities')
+        .select()
+        .match({ user_id: client.auth.user().id, })
+        .single();
+
+    return checkError(response);    
+}
+
+export async function createCity(city){
+    const response = await client
+        .from('cities')
+        .insert({
+            ...city, 
+            user_id: client.auth.user().id, 
+        })
+        .single();
+
+    return checkError(response);
+}
+
+export async function updateWaterfront(value){
+    const currentUserId = client.auth.user().id;
+
+    // in supabase, update the waterfront property
+    // for the city whose user_id match's the currently logged in user's id
+    const response = await client
+        .from('cities')
+        .update({ waterfront: value })
+        .match({ user_id: currentUserId });    
+
+    return checkError(response);    
+}
+
+export async function updateSkyline(value){
+    const currentUserId = client.auth.user().id;
+
+    // in supabase, update the skyline property
+    // for the city whose user_id match's the currently logged in user's id
+    const response = await client
+        .from('cities')
+        .update({ skyline: value })
+        .match({ user_id: currentUserId });    
+
+    return checkError(response);    
+}
+
+
+export async function updateCastle(value){
+    const currentUserId = client.auth.user().id;
+
+    // in supabase, update the castle property
+    // for the city whose user_id match's the currently logged in user's id
+    const response = await client
+        .from('cities')
+        .update({ castle: value })
+        .match({ user_id: currentUserId });    
+
+    return checkError(response);    
+}
+
+export async function updateSlogans(value){
+    const currentUserId = client.auth.user().id;
+
+    // in supabase, update the slogans property
+    // for the city whose user_id match's the currently logged in user's id
+    const response = await client
+        .from('cities')
+        .update({ slogans: value })
+        .match({ user_id: currentUserId });    
+
+    return checkError(response);    
+}
+
+export async function updateName(value){
+    const currentUserId = client.auth.user().id;
+
+    // in supabase, update the name property
+    // for the city whose user_id match's the currently logged in user's id
+    const response = await client
+        .from('cities')
+        .update({ name: value })
+        .match({ user_id: currentUserId });    
+
+    return checkError(response);    
+}
+
+// everything below here should be good from the template
 export async function getUser() {
     return client.auth.session();
 }
-
 
 export async function checkAuth() {
     const user = await getUser();
--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,4 +1,4 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, logout, fetchCity, updateWaterfront, updateSkyline, updateCastle, updateSlogans, createCity } from '../fetch-utils.js';
 
 checkAuth();
 
@@ -7,3 +7,24 @@ const logoutButton = document.getElementById('logout');
 logoutButton.addEventListener('click', () => {
     logout();
 });
+// WIP begins here
+async function displayCity(city) {
+    // fetch the city from supabase
+    const { waterfront, skyline, castle } = await fetchCity();
+    // if the city has a waterfront, display the waterfront in the dom
+    if (waterfront) waterfrontEl.style.backgroundImage = `url("../assets/${waterfront}-waterfront.png")`;
+    // if the city has a skyline, display the skyline in the dom
+    if (skyline) skylineEl.style.backgroundImage = `url("../assets/${skyline}-skyline.png")`;
+    // if the city has a castle, display the castle in the dom
+    if (castle) castleEl.style.backgroundImage = `url("../assets/${castle}-castle.png")`;
+    // loop through slogans and display them to the dom (clearing out old dom if necessary)
+    slogansEl.textContent = '';
+    for (let slogan of slogans) {
+        const p = document.createElement('p');
+
+        p.classList.add('slogan');
+        p.textContent = slogan;
+
+        slogansEl.append(p);
+    }
+}
