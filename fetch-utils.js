3ed95dcd5da04890d71c038a63e8ba0b3ae4f731
Benatovich <sambenatovich@gmail.com>
2022-01-04 18:30:14 -0500

    login/logout all functional, supabase table is set up, html should be good

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://cnfgxcsilmdanstiixji.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTUwNzY3MywiZXhwIjoxOTU1MDgzNjczfQ.T4j8T8PmMQvcyTO4egZ0EUIxHwmUeklr4DArTmn6mc0';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
