ea437b214cead28ecef2acbe5ad9ad80c1893a5a
Benatovich <sambenatovich@gmail.com>
2022-01-05 18:12:12 -0500

    radically changed html. need to alter css and js to fit

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -15,16 +15,16 @@ const waterfrontImageEl = document.querySelector('#waterfront-image');
 const castleImageEl = document.querySelector('#castle-image');
 
 // const sloganButton = document.querySelector('#slogan-button');
-const sloganListEl = document.querySelector('.list');
+const sloganListEl = document.querySelector('.slogan-list');
 
 const countEl = document.querySelector('.count');
 const logoutButton = document.getElementById('logout');
 
-// // we're still keeping track of 'this session' clicks, so we keep these lets
-// let nameCount = 0;
-// let waterfrontCount = 0;
-// let skylineCount = 0;
-// let castleCount = 0;
+// if we want to track 'this session' changes:
+let nameCount = 0;
+let waterfrontCount = 0;
+let skylineCount = 0;
+let castleCount = 0;
 // let slogans = [];
 
 logoutButton.addEventListener('click', () => {
@@ -33,92 +33,119 @@ logoutButton.addEventListener('click', () => {
 
 waterfrontDropdown.addEventListener('change', async() => {
     // increment the correct count in state
-    waterfrontCount++;
+    // waterfrontCount++;
     // update the waterfront in supabase with the correct data
-    await updateWaterfront(waterfrontDropdown.value);
-    refreshData();
+    const updatedCity = await updateWaterfront(waterfrontDropdown.value);
+    // refreshData();
+    displayCity(updatedCity);
+    displayStats();
 });
 
 
 skylineDropdown.addEventListener('change', async() => {
     // increment the correct count in state
-    skylineCount++;
+    // skylineCount++;
     // update the skyline in supabase with the correct data
-    await updateSkyline(skylineDropdown.value);
-    refreshData();
+    const updatedCity = await updateSkyline(skylineDropdown.value);
+    // refreshData();
+    displayCity(updatedCity);
+    displayStats();
+
 });
 
 
 castleDropdown.addEventListener('change', async() => {
     // increment the correct count in state
-    castleCount++;
+    // castleCount++;
     // update the castle in supabase with the correct data
-    await updateCastle(castleDropdown.value);
-    refreshData();
+    const updatedCity = await updateCastle(castleDropdown.value);
+    // refreshData();
+    displayCity(updatedCity);
+    displayStats();
 });
 
-sloganButton.addEventListener('click', async() => {
-    // go fetch the old slogans
-    // ^do i need anything for this here?
-    // update the slogan array locally by pushing the new slogan into the old array
-    slogans.push(sloganInput.value);
+nameForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
 
-    sloganInput.value = '';
-    // update the slogans in supabase by passing the mutated array to the updateCatchphrases function
-    await updateSlogans(slogans);
+    const data = new FormData(nameForm);
 
-    refreshData();
+    const name = data.get('name');
+
+    const updatedCity = await updateName(name);
+
+    displayCity(updatedCity);
+    displayStats();
 });
+
+sloganForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const data = new FormData(sloganForm);
+// get the new slogan from the form
+    const newSlogan = data.get('slogan');
+
+    // get the old city/slogans from supabase
+    const city = await fetchCity();
+
+    // push new slogan into array of existing slogans
+    city.slogans.push(newSlogan);
+
+    const updatedCity = await updateSlogans(city.slogans);
+
+    displayCity(updatedCity);
+    displayStats();
+
+});
+
+// no more sloganButton
+// sloganButton.addEventListener('click', async() => {
+//     // go fetch the old slogans
+//     // ^do i need anything for this here?
+//     // update the slogan array locally by pushing the new slogan into the old array
+//     slogans.push(sloganInput.value);
+
+//     sloganInput.value = '';
+//     // update the slogans in supabase by passing the mutated array to the updateCatchphrases function
+//     await updateSlogans(slogans);
+
+//     refreshData();
+// });
 
 window.addEventListener('load', async() => {
     // on load, attempt to fetch this user's city
     let city = await fetchCity();
 
-    // if this user turns out not to have a city
-    // create a new city with correct defaults for all properties (name, waterfront, skyline, castle, slogans)
+    // if user doesn't have one, make a new city with correct defaults
     if (!city) {
-        city = await createCity({
-            name: '',
-            waterfront: '',
-            skyline: '',
-            castle: '',
-            slogans: [],
-        });
+        const newCity = await createCity();
+
+        displayCity(newCity);
+    } else {
+        displayCity(city);
     }
-    // and put the city's slogans in state (we'll need to hold onto them for an interesting reason);
-    slogans = city.slogans;
-    // then call the refreshData function to set the DOM with the updated data
-    refreshData();
+    displayStats();
 });
-
-
 
 function displayStats() {
     countEl.textContent = `In this session, you have changed the name ${nameCount} times, the waterfront ${waterfrontCount} times, the skyline ${skylineCount} times, and the castle ${castleCount} times. And nobody can forget your city's classic slogans:`;
 }
 
-async function displayCity(city) {
-    // fetch the character from supabase
-    const city = await fetchCity();
-    // if the character has a head, display the head in the dom
-    if (head) headEl.style.backgroundImage = `url("../assets/${head}-head.png")`;
-    // if the character has a middle, display the middle in the dom
-    if (middle) middleEl.style.backgroundImage = `url("../assets/${middle}-middle.png")`;
-    // if the character has a pants, display the pants in the dom
-    if (bottom) bottomEl.style.backgroundImage = `url("../assets/${bottom}-pants.png")`;
-    // loop through catchphrases and display them to the dom (clearing out old dom if necessary)
-    catchphrasesEl.textContent = '';
-    for (let catchphrase of catchphrases) {
-        const p = document.createElement('p');
+function displayCity(city) {
+    cityNameEl.textContent = city.name;
+    waterfrontImageEl.src = `../assets/${city.waterfront_id}-waterfront.jpg`;
+    skylineImageEl.src = `../assets/${city.skyline_id}-skyline.jpg`;
+    castleImageEl.src = `../assets/${city.castle_id}-castle.jpg`;
 
-        p.classList.add('catchphrase');
-        p.textContent = catchphrase;
+    // loop through slogans, render, append
+    sloganListEl.textContent = '';
 
-        catchphrasesEl.append(p);
+    for (let slogan of city.slogans) {
+        const sloganEl = document.createElement('p');
+
+        sloganEl.classList.add('slogan');
+
+        sloganEl.textContent = slogan;
+
+        sloganListEl.append(sloganEl);
     }
-}
-
-function refreshData() {
-    displayStats();
-    fetchAndDisplayCharacter();
 }
